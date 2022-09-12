456e5cdcc87562712fbb6b6d2ec96414da6a27e6
yjuwaheer <yjuwaheer@mun.ca>
2022-09-12 13:45:43 -0230

    Added projects template and Tracker component

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+// Components
+import Tracker from "../components/projects/Tracker";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div>
+      <div className="w-full bg-gray-100 h-96 rounded-xl">
+        <Tracker />
+      </div>
+      <div className="flex mt-10">
+        <div className="w-1/2 mr-10 bg-gray-100 h-96 rounded-xl"></div>
+        <div className="w-1/2 bg-gray-100 h-96 rounded-xl"></div>
+      </div>
+      <div className="flex mt-10">
+        <div className="w-2/3 mr-10 bg-gray-100 h-96 rounded-xl"></div>
+        <div className="w-1/3 bg-gray-100 h-96 rounded-xl"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
