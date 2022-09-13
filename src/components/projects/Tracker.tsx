a6684585496c171d57d387fe1306d8b3d7582b61
yjuwaheer <yjuwaheer@mun.ca>
2022-09-12 23:36:07 -0230

    Updated tracker component

--- src/components/projects/Tracker.tsx
+++ src/components/projects/Tracker.tsx
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 // Tooltip
 import ReactTooltip from "react-tooltip";
 // Icons
 import { IoLogoElectron } from "react-icons/io5";
-import { ImArrowUpRight2 } from "react-icons/im";
 import { FiDownload } from "react-icons/fi";
 import { BsWindows } from "react-icons/bs";
 // Icons Demo App
@@ -14,8 +13,11 @@ import { GoProject } from "react-icons/go";
 import { FcTodoList } from "react-icons/fc";
 
 const Tracker = () => {
+  // States
+  const [tab, setTab] = useState("dashboard");
+
   return (
-    <div>
+    <div className="overflow-hidden">
       <div className="flex items-center justify-between">
         <div>
           <div className="mb-1 text-2xl font-bold">Tracker App</div>
@@ -36,38 +38,79 @@ const Tracker = () => {
         </a>
       </div>
       <div className="my-3 border-b border-gray-200"></div>
-      {/* <img
-        src="/images/tracker.png"
-        alt="tracker"
-        className="border border-gray-200 rounded-xl"
-      /> */}
 
       {/* Demo App */}
-      <div className="flex flex-col justify-between w-48 bg-white rounded-l-xl">
+      <div className="flex">
+        {/* Menu */}
+        <div className="flex flex-col justify-between bg-white rounded-l-xl">
+          <div>
+            <div className="flex items-center justify-center h-24 mb-4 text-xl font-bold bg-gray-50 rounded-l-xl">
+              <FcTodoList className="text-4xl" />
+            </div>
+
+            <div
+              className="flex items-center px-8 py-2 mx-3 my-2 font-semibold text-gray-600 outline-none cursor-pointer rounded-xl hover:bg-red-500 hover:text-white"
+              onClick={() => setTab("dashboard")}
+            >
+              <BiHome />
+              <div className="pl-1">Dashboard</div>
+            </div>
+
+            <div
+              className="flex items-center px-8 py-2 mx-3 my-2 font-semibold text-gray-600 outline-none cursor-pointer rounded-xl hover:bg-red-500 hover:text-white"
+              onClick={() => setTab("tasks")}
+            >
+              <FaTasks />
+              <div className="pl-1">Tasks</div>
+            </div>
+
+            <div
+              className="flex items-center px-8 py-2 mx-3 my-2 font-semibold text-gray-600 outline-none cursor-pointer rounded-xl hover:bg-red-500 hover:text-white"
+              onClick={() => setTab("projects")}
+            >
+              <GoProject />
+              <div className="pl-1">Projects</div>
+            </div>
+
+            <div
+              className="flex items-center px-8 py-2 mx-3 my-2 font-semibold text-gray-600 outline-none cursor-pointer rounded-xl hover:bg-red-500 hover:text-white"
+              onClick={() => setTab("settings")}
+            >
+              <FiSettings />
+              <div className="pl-1">Settings</div>
+            </div>
+          </div>
+        </div>
+        {/* Demo App Sections */}
         <div>
-          <div className="flex items-center justify-center h-24 mb-4 text-xl font-bold bg-gray-50 rounded-l-xl">
-            <FcTodoList className="text-4xl" />
-          </div>
-
-          <div className="flex items-center px-3 py-2 mx-3 my-2 font-semibold text-gray-600 outline-none cursor-pointer rounded-xl hover:bg-red-500 hover:text-white">
-            <BiHome />
-            <div className="pl-1">Dashboard</div>
-          </div>
-
-          <div className="flex items-center px-3 py-2 mx-3 my-2 font-semibold text-gray-600 outline-none cursor-pointer rounded-xl hover:bg-red-500 hover:text-white">
-            <FaTasks />
-            <div className="pl-1">Tasks</div>
-          </div>
-
-          <div className="flex items-center px-3 py-2 mx-3 my-2 font-semibold text-gray-600 outline-none cursor-pointer rounded-xl hover:bg-red-500 hover:text-white">
-            <GoProject />
-            <div className="pl-1">Projects</div>
-          </div>
-
-          <div className="flex items-center px-3 py-2 mx-3 my-2 font-semibold text-gray-600 outline-none cursor-pointer rounded-xl hover:bg-red-500 hover:text-white">
-            <FiSettings />
-            <div className="pl-1">Settings</div>
-          </div>
+          {tab === "dashboard" && (
+            <img
+              src="/images/tracker1.png"
+              alt="tracker1"
+              className="rounded-r-xl"
+            />
+          )}
+          {tab === "tasks" && (
+            <img
+              src="/images/tracker2.png"
+              alt="tracker2"
+              className="rounded-r-xl"
+            />
+          )}
+          {tab === "projects" && (
+            <img
+              src="/images/tracker3.png"
+              alt="tracker3"
+              className="rounded-r-xl"
+            />
+          )}
+          {tab === "settings" && (
+            <img
+              src="/images/tracker4.png"
+              alt="tracker4"
+              className="rounded-r-xl"
+            />
+          )}
         </div>
       </div>
 
