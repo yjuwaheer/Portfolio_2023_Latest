dcf9184f320a865da22254d462f724514c152426
yjuwaheer <yjuwaheer@mun.ca>
2022-09-14 15:06:36 -0230

    Added board canvas clear

--- src/components/projects/Board.tsx
+++ src/components/projects/Board.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
+// React sketch canvas
+import { ReactSketchCanvas, ReactSketchCanvasRef } from "react-sketch-canvas";
 // Tooltip
 import ReactTooltip from "react-tooltip";
 // Icons
-import { BiChalkboard } from "react-icons/bi";
+import { FaChalkboard } from "react-icons/fa";
 import { FaNodeJs } from "react-icons/fa";
 import { ImArrowUpRight2 } from "react-icons/im";
 // Icons Demo App
-import { MdChangeCircle } from "react-icons/md";
+// Icons Demo App
+import { GrPowerReset } from "react-icons/gr";
+import { IoMdSave } from "react-icons/io";
 
 const Board = () => {
+  // Hooks
+  const canvasRef = useRef<ReactSketchCanvasRef>(null);
+
   return (
     <div className="relative overflow-hidden">
       <div className="flex items-center justify-between">
@@ -32,13 +39,43 @@ const Board = () => {
       </div>
       <div className="my-3 border-b border-gray-200"></div>
 
+      {/* Menu */}
+      <div className="absolute z-50 flex -translate-x-1/2 top-[21rem] left-1/2 bg-gray-100 rounded-xl border border-gray-200">
+        <div
+          className={`px-2 py-1 mr-1 rounded-full cursor-pointer hover:bg-gray-300`}
+          onClick={() => {
+            canvasRef?.current?.clearCanvas();
+          }}
+        >
+          <GrPowerReset />
+        </div>
+        <div
+          className={`px-2 py-1 rounded-full cursor-pointer hover:bg-gray-300`}
+          onClick={() => {}}
+        >
+          <IoMdSave />
+        </div>
+      </div>
+
       {/* Demo App */}
-      <div className="flex justify-center"></div>
+      <div className="flex justify-center">
+        <div className="w-3/4 border-8 border-gray-700 rounded-xl">
+          <div className="border-4 border-gray-500 rounded-sm h-96">
+            <ReactSketchCanvas
+              ref={canvasRef}
+              style={{ border: "none" }}
+              strokeWidth={4}
+              strokeColor="black"
+              backgroundImage="/images/board_grid.jpg"
+            />
+          </div>
+        </div>
+      </div>
 
       {/* Tooltip Component */}
       <ReactTooltip id="board">
         <span className="flex items-center">
-          View App <BiChalkboard className="mx-2" />
+          View App <FaChalkboard className="mx-2" />
         </span>
       </ReactTooltip>
     </div>
