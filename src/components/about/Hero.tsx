5c037d4ff98dbcb3d51833678dba55e804a64dc1
yjuwaheer <yjuwaheer@mun.ca>
2022-09-14 23:19:01 -0230

    Crafted about page hero and highlights

---
+++ src/components/about/Hero.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+// Icons
+import { HiCursorClick } from "react-icons/hi";
+
+const Hero = () => {
+  return (
+    <div className="flex justify-center">
+      {/* Avatar and cursors */}
+      <div className="relative w-max">
+        <img src="notion_avatar_transparent_cropped.png" alt="avatar" />
+        <div className="absolute flex bottom-10 -right-10">
+          <HiCursorClick className="mr-1 text-gray-700 " />
+          <div className="px-2 text-sm font-medium text-gray-600 bg-gray-100 rounded-xl">
+            Developer
+          </div>
+        </div>
+        <div className="absolute flex bottom-24 -left-12">
+          <div className="px-2 text-sm font-medium text-gray-600 bg-gray-100 rounded-xl">
+            Student
+          </div>
+          <HiCursorClick className="ml-1 text-gray-700 rotate-90" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+// Components
+import Hero from "../components/about/Hero";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <Hero />
+
+      {/* Hightlights */}
+      <div className="flex flex-col items-center">
+        <div className="text-4xl font-semibold text-center">Hi, I'm Yudish</div>
+        <div className="px-2 my-2 font-medium text-gray-700 bg-gray-100 border rounded-xl">Currently a Final year student at MUN</div>
+        
+        <div className="font-semibold mt-14 text-8xl">Passionate</div>
+        <div className="mt-4 text-8xl">to craft amazing</div>
+        <div className="mt-4 text-8xl">websites</div>
+
+
+      </div>
+    </div>
+  );
 };
 
 export default About;
