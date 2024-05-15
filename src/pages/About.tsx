252a553242589d558c33388adf3026244e624fee
Yudish Juwaheer <60716609+yjuwaheer@users.noreply.github.com>
2024-05-15 19:29:44 -0230

    Updated current role in About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -37,7 +37,7 @@ const About = () => {
           } border rounded-xl
           `}
         >
-          Currently a Technical Consultant
+          Currently a Software Developer
         </div>
 
         <div
